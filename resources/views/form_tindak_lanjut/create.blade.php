738f7d0eb1d6ef5e366fbd09b73f2a5ecc8623f1
romichoirudin33 <romichoirudin33@gmail.com>
2022-12-08 15:19:16 +0800

    fix

--- resources/views/form_tindak_lanjut/create.blade.php
+++ resources/views/form_tindak_lanjut/create.blade.php
@@ -22,43 +22,52 @@
                                         <div class="col-md-4">
                                             <div class="mb-3">
                                                 <label for="kode_temuan">Kode Temuan</label>
-                                                <input type="text" id="kode_temuan" name="kode_temuan" class="form-control shadow-none" value="{{ $data['kode_temuan'] }}">
+                                                <input type="text" id="kode_temuan" name="kode_temuan"
+                                                    class="form-control shadow-none" value="{{ $data['kode_temuan'] }}">
                                             </div>
                                         </div>
                                         <div class="col-md-4">
                                             <div class="mb-3">
                                                 <label for="kode_rekomendasi">Kode Rekomendasi</label>
-                                                <input type="text" id="kode_rekomendasi" name="kode_rekomendasi" class="form-control shadow-none" value="{{ $data['kode_rekomendasi'] }}">
+                                                <input type="text" id="kode_rekomendasi" name="kode_rekomendasi"
+                                                    class="form-control shadow-none"
+                                                    value="{{ $data['kode_rekomendasi'] }}">
                                             </div>
                                         </div>
                                         <div class="col-md-4">
                                             <div class="mb-3">
                                                 <label for="kode_lha">Kode LHA</label>
-                                                <input type="text" id="kode_lha" name="kode_lha" class="form-control shadow-none" value="{{ $data['kode_lha'] }}">
+                                                <input type="text" id="kode_lha" name="kode_lha"
+                                                    class="form-control shadow-none" value="{{ $data['kode_lha'] }}">
                                             </div>
                                         </div>
                                     </div>
                                     <div class="mb-3">
                                         <label for="tindak_lanjut">Tindak Lanjut</label>
                                         <textarea name="tindak_lanjut" id="tindak_lanjut" rows="5" class="form-control shadow-none"></textarea>
-                                        <span class="text-muted">Anda dapat menambahkan link bukti tindak lanjut disini (seperti GDrive, atau lainnya)</span>
+                                        <span class="text-muted">Anda dapat menambahkan link bukti tindak lanjut disini
+                                            (seperti GDrive, atau lainnya)</span>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
-                                    <h6 class="fw-bold">Penanggungjawab Temuan</h6>
+                                    <h6 class="fw-bold">Penanggung Jawab</h6>
                                     <div class="mb-3">
                                         <label class="form-label">Cari pegawai</label>
                                         <div class="input-group">
-                                            <input type="text" class="form-control shadow-none" id="q" name="q" value="{{ request()->get('q') }}">
-                                            <button class="btn btn-outline-secondary" type="button" onclick="getPegawai()">Cari</button>
+                                            <input type="text" class="form-control shadow-none" id="q"
+                                                name="q" value="{{ request()->get('q') }}">
+                                            <button class="btn btn-outline-secondary" type="button"
+                                                onclick="getPegawai()">Cari</button>
                                         </div>
-                                        <div class="form-text">Masukkan NIP/Nama untuk melakukan pencarian. Source data Si-Mutiara</div>
+                                        <div class="form-text">Masukkan NIP/Nama untuk melakukan pencarian. Source data
+                                            Si-Mutiara</div>
                                         <div class="text-danger" id="msg_error"></div>
                                     </div>
                                     <div class="mb-3">
-                                        @foreach($data_pegawai as $pegawai)
+                                        @foreach ($data_pegawai as $pegawai)
                                             <div class="form-check mb-2">
-                                                <input class="form-check-input" type="radio" name="nip" id="{{ $pegawai['nip'] }}" value="{{ $pegawai['nip'] }}">
+                                                <input class="form-check-input" type="radio" name="nip"
+                                                    id="{{ $pegawai['nip'] }}" value="{{ $pegawai['nip'] }}">
                                                 <label class="form-check-label" for="{{ $pegawai['nip'] }}">
                                                     <b>{{ $pegawai['nip'] }}</b> -
                                                     {{ $pegawai['nama'] }} <br>
@@ -73,18 +82,18 @@
 
 
 
-{{--                            <div class="mb-3">--}}
-{{--                                <label for="nip">NIP</label>--}}
-{{--                                <input type="text" id="nip" name="nip" class="form-control shadow-none" value="">--}}
-{{--                            </div>--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label for="nama">Nama</label>--}}
-{{--                                <input type="text" id="nama" name="nama" class="form-control shadow-none" value="">--}}
-{{--                            </div>--}}
-{{--                            <div class="mb-3">--}}
-{{--                                <label for="jabatan">Jabatan</label>--}}
-{{--                                <input type="text" id="jabatan" name="jabatan" class="form-control shadow-none" value="">--}}
-{{--                            </div>--}}
+                            {{--                            <div class="mb-3"> --}}
+                            {{--                                <label for="nip">NIP</label> --}}
+                            {{--                                <input type="text" id="nip" name="nip" class="form-control shadow-none" value=""> --}}
+                            {{--                            </div> --}}
+                            {{--                            <div class="mb-3"> --}}
+                            {{--                                <label for="nama">Nama</label> --}}
+                            {{--                                <input type="text" id="nama" name="nama" class="form-control shadow-none" value=""> --}}
+                            {{--                            </div> --}}
+                            {{--                            <div class="mb-3"> --}}
+                            {{--                                <label for="jabatan">Jabatan</label> --}}
+                            {{--                                <input type="text" id="jabatan" name="jabatan" class="form-control shadow-none" value=""> --}}
+                            {{--                            </div> --}}
                             <div class="mb-3">
                                 <button type="submit" class="btn btn-primary">
                                     Simpan
@@ -103,18 +112,18 @@
 
 @section('js')
     <script>
-        function getPegawai(){
+        function getPegawai() {
             let cari = document.getElementById('q').value;
             let msg_error = document.getElementById('msg_error');
             msg_error.innerHTML = '';
-            if (cari.length === 0){
+            if (cari.length === 0) {
                 msg_error.innerHTML = 'Anda belum memasukkan pencarian';
-            }else{
+            } else {
                 let kode_rekomendasi = '{{ request()->get('kode_rekomendasi') }}';
-                location.replace('{{ route('tindak_lanjut_create') }}?kode_rekomendasi='+kode_rekomendasi+'&q='+cari);
+                location.replace('{{ route('tindak_lanjut_create') }}?kode_rekomendasi=' + kode_rekomendasi + '&q=' +
+                cari);
                 // console.log();
             }
         }
     </script>
 @endsection
-
