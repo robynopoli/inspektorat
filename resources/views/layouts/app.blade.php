dfc6a5fdedac95440271f483e6b07c991b112132
romichoirudin33 <romichoirudin33@gmail.com>
2022-12-11 09:27:06 +0800

    tambahkan chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -191,6 +191,23 @@
         </div>
     </footer>
 </div>
+<div class="chart" id="basic-example"></div>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement('script'),
+            s0 = document.getElementsByTagName('script')[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/636b364ddaff0e1306d6780e/1ghdco7tm';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 
 @yield('js')
 </body>
