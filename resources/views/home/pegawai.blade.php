122f2d877adf7c80a34536da234c2fb055615eb5
romichoirudin33 <romichoirudin33@gmail.com>
2022-12-11 23:16:47 +0800

    tambahkan link

--- resources/views/home/pegawai.blade.php
+++ resources/views/home/pegawai.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <div class="container">
-        <h1 class="mt-3 mb-5 text-center text-uppercase animate__animated animate__zoomIn" style="font-weight: bolder; font-size: 4em; font-family: 'Segoe UI'">
+        <h1 class="text-uppercase animate__animated animate__zoomIn mt-3 mb-5 text-center"
+            style="font-weight: bolder; font-size: 4em; font-family: 'Segoe UI'">
             S A P A
             <span class="text-gradient">&emsp14; Inspektur</span>
         </h1>
@@ -12,70 +13,57 @@
                 <h4 class="text-capitalize fw-bold">
                     imtak pengawasan
                 </h4>
-                <p class="mb-3 text-muted pe-5">
+                <p class="text-muted pe-5 mb-3">
                     Layanan konsultasi Obrik untuk pembinaan penyelenggaraan pemerintahan
                 </p>
-                <a href=""
-                   class="btn btn-primary rounded-3 px-4">
+                <a href="{{ route('pegawai.imtak-pengawasan.index') }}" class="btn btn-primary rounded-3 px-4">
                     Masuk Aplikasi
-                    {{--                    <div class="">--}}
-                    {{--                        <svg--}}
-                    {{--                            aria-hidden="true"--}}
-                    {{--                            class=""--}}
-                    {{--                            fill="currentColor"--}}
-                    {{--                            viewBox="0 0 120 20"--}}
-                    {{--                            xmlns="http://www.w3.org/2000/svg">--}}
-                    {{--                            <path--}}
-                    {{--                                fill-rule="evenodd"--}}
-                    {{--                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"--}}
-                    {{--                                clip-rule="evenodd"--}}
-                    {{--                            />--}}
-                    {{--                        </svg>--}}
-                    {{--                    </div>--}}
+                    {{--                    <div class=""> --}}
+                    {{--                        <svg --}}
+                    {{--                            aria-hidden="true" --}}
+                    {{--                            class="" --}}
+                    {{--                            fill="currentColor" --}}
+                    {{--                            viewBox="0 0 120 20" --}}
+                    {{--                            xmlns="http://www.w3.org/2000/svg"> --}}
+                    {{--                            <path --}}
+                    {{--                                fill-rule="evenodd" --}}
+                    {{--                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" --}}
+                    {{--                                clip-rule="evenodd" --}}
+                    {{--                            /> --}}
+                    {{--                        </svg> --}}
+                    {{--                    </div> --}}
                 </a>
-                <img
-                    class="max-w-full h-auto"
-                    src="{{ asset('assets/coaching.gif') }}"
-                    alt="coaching"
-                />
+                <img class="h-auto max-w-full" src="{{ asset('assets/coaching.gif') }}" alt="coaching" />
             </div>
             <div class="col-md-6 animate__animated animate__fadeInUp">
                 <h4 class="text-capitalize fw-bold">
                     Pemantauan Tindak Lanjut
                 </h4>
-                <p class="mb-3 text-muted ">
+                <p class="text-muted mb-3">
                     Aplikasi pemantauan tindak lanjut terintegrasi dengan SIMHPNAS
                 </p>
-                <a href="{{ route('pemantauan-tindak-lanjut.index') }}"
-                   class="btn btn-primary rounded-3 px-4">
+                <a href="{{ route('pemantauan-tindak-lanjut.index') }}" class="btn btn-primary rounded-3 px-4">
                     Masuk Aplikasi
-                    {{--                    <div class="">--}}
-                    {{--                        <svg--}}
-                    {{--                            aria-hidden="true"--}}
-                    {{--                            class=""--}}
-                    {{--                            fill="currentColor"--}}
-                    {{--                            viewBox="0 0 120 20"--}}
-                    {{--                            xmlns="http://www.w3.org/2000/svg">--}}
-                    {{--                            <path--}}
-                    {{--                                fill-rule="evenodd"--}}
-                    {{--                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"--}}
-                    {{--                                clip-rule="evenodd"--}}
-                    {{--                            />--}}
-                    {{--                        </svg>--}}
-                    {{--                    </div>--}}
+                    {{--                    <div class=""> --}}
+                    {{--                        <svg --}}
+                    {{--                            aria-hidden="true" --}}
+                    {{--                            class="" --}}
+                    {{--                            fill="currentColor" --}}
+                    {{--                            viewBox="0 0 120 20" --}}
+                    {{--                            xmlns="http://www.w3.org/2000/svg"> --}}
+                    {{--                            <path --}}
+                    {{--                                fill-rule="evenodd" --}}
+                    {{--                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" --}}
+                    {{--                                clip-rule="evenodd" --}}
+                    {{--                            /> --}}
+                    {{--                        </svg> --}}
+                    {{--                    </div> --}}
                 </a>
-                <img
-                    class="max-w-full h-auto"
-                    src="{{ asset('assets/ptl.gif') }}"
-                    alt="coaching"
-                />
+                <img class="h-auto max-w-full" src="{{ asset('assets/ptl.gif') }}" alt="coaching" />
             </div>
         </div>
-        @endsection
+    @endsection
 
-        @section('css')
-            <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-            />
-@endsection
+    @section('css')
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    @endsection
